7715b8037dac53a6187478f5b136fd6a1b700f25
Max <Max@DESKTOP-RFT814J>
2022-06-23 14:52:50 +0200

    Add project files.

---
+++ InlämningDatalogiME/BinarySearchTree.cs
@@ -0,0 +1,29 @@
+﻿
+namespace InlämningDatalogiME
+{
+    public class BinarySearchTree<T> : BST_G<T> where T : IComparable<T>
+    {
+        private Node<T>? Root = null;
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T value)
+        {
+            if (Root == null) Root = new Node<T>(value); return;
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(Root.Data);
+        }
+    }
+}
---
+++ InlämningDatalogiME/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using InlämningDatalogiME;
+
+BinarySearchTree<int> lista = new BinarySearchTree<int>();
+lista.Insert(1);
+lista.Print();
